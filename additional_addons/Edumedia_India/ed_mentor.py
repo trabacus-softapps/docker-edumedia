c9063a74adfb45a1a844a79abbce385444790f51
arun-keshav <arun.keshav@trabacus.com>
2015-12-07 14:20:18 +0530

    Email

--- additional_addons/Edumedia_India/ed_mentor.py
+++ additional_addons/Edumedia_India/ed_mentor.py
@@ -576,6 +576,37 @@ class stock_picking(osv.osv):
                 'courier_name': fields.char('Courier Name', size=100),
                 }
     _order = 'id desc'
+
+    def action_quotation_send(self, cr, uid, ids, context=None):
+        '''
+        This function opens a window to compose an email, with the edi sale template message loaded by default
+        '''
+        assert len(ids) == 1, 'This option should only be used for a single id at a time.'
+        ir_model_data = self.pool.get('ir.model.data')
+        template_id = False
+        try:
+            compose_form_id = ir_model_data.get_object_reference(cr, uid, 'mail', 'email_compose_message_wizard_form')[1]
+        except ValueError:
+            compose_form_id = False
+        ctx = dict()
+        ctx.update({
+            'default_model': 'stock.picking',
+            'default_res_id': ids[0],
+            'default_use_template': bool(template_id),
+            'default_template_id': template_id,
+            'default_composition_mode': 'comment',
+            'mark_so_as_sent': True
+        })
+        return {
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'mail.compose.message',
+            'views': [(compose_form_id, 'form')],
+            'view_id': compose_form_id,
+            'target': 'new',
+            'context': ctx,
+        }
 stock_picking()
 
 class sale_order(osv.osv):
